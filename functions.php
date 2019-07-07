3f29a22f61581cf5fb7981b08b5736294332c6bc
Srikanth Kamath <Srikanth@tskamath.com>
2019-07-07 12:05:31 +0530

    added files for webpack

---
+++ functions.php
@@ -0,0 +1,26 @@
+<?php
+
+function tskwoo_enqueue_stylesheets () {
+	$handle = 'tskwoo-stylesheet';
+	$src 	= get_template_directory_uri() . '/style.css';
+	$deps 	= '';
+	$ver 	= '2019July';
+	$media 	= 'all';
+	wp_register_style( $handle, $src, $deps, $ver, $media );
+	wp_enqueue_style('tskwoo-stylesheet');
+}
+add_action( 'wp_enqueue_scripts', 'tskwoo_enqueue_stylesheets' );
+
+
+function tskwoo_enqueue_scripts() {
+	$handle = 'tskwoo-js';
+	$src 	= get_template_directory_uri() . '/app.js';
+	$deps 	= 'jquery';
+	$ver 	= '2019July';
+	$media 	= 'all';
+	wp_enqueue_script( $handle, $src, $deps , $ver , true);
+	wp_enqueue_script('tskwoo-js');
+}
+add_action( 'wp_enqueue_scripts', 'tskwoo_enqueue_scripts' );
+
+
--- notes.md
+++ notes.md
@@ -4,34 +4,30 @@ Install Node.js
 
 ## github
 > echo "# wootheme" >> README.md
-git init
-git add *
-git commit -m "Intial Files"
-git remote add origin https://github.com/tsktech/wootheme.git
+> git init
+> git add *
+> git commit -m "Intial Files"
+> git remote add origin https://github.com/tsktech/wootheme.git
 
 
 https://developer.wordpress.org/themes/basics/main-stylesheet-style-css/
-
 screenshot.png 1200 × 900
-
 touch index.php
-
 touch style.css
-
 touch header.php
-
 touch footer.php
-
 touch functions.php
-
 touch page.php
-
 touch front-page.php
-
 touch single.php
-
 touch archive.php
-
 touch 404.php
+
+https://github.com/laravel/laravel
+package.json
+webpack.mix.js
+
+npm install
+
 
 
